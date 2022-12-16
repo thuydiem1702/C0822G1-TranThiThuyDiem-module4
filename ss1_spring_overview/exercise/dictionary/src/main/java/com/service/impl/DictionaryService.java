1fa03860e89168ff5d6a94f227103c5048661c8d
thuydiem1702 <thuydiem17022001@gmail.com>
2022-12-16 11:30:35 +0700

    up

--- ss1_spring_overview/exercise/dictionary/src/main/java/com/service/impl/DictionaryService.java
+++ ss1_spring_overview/exercise/dictionary/src/main/java/com/service/impl/DictionaryService.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @Service
 public class DictionaryService implements IDictionaryService {
-    public static Map<String, String> dictionary = new HashMap<>();
+    private static Map<String, String> dictionary = new HashMap<>();
 
     static {
         dictionary.put("watermelon", "dưa hấu");

556472fa6a674725945eb88430a5f0aeee09fe62
thuydiem1702 <thuydiem17022001@gmail.com>
2022-12-22 18:21:44 +0700

    up

---
+++ ss7_spring_boot/exercise/blog_vip/src/main/java/com/example/blog_vip/BlogVipApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_vip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogVipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogVipApplication.class, args);
+    }
+
+}
--- ss7_spring_boot/exercise/blogg/src/main/java/com/example/model/Blog.java
+++ ss7_spring_boot/exercise/blogg/src/main/java/com/example/model/Blog.java
@@ -1,7 +1,6 @@
 package com.example.model;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 
 @Entity
 public class Blog {

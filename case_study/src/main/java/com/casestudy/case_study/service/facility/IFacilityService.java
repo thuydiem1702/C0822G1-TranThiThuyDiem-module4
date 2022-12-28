b2dab89a8343948aa3fd7dfc21b5d2b99d2ba6ef
thuydiem1702 <thuydiem17022001@gmail.com>
2022-12-28 22:07:39 +0700

    up

--- case_study/src/main/java/com/casestudy/case_study/service/facility/IFacilityService.java
+++ case_study/src/main/java/com/casestudy/case_study/service/facility/IFacilityService.java
@@ -1,13 +1,11 @@
 package com.casestudy.case_study.service.facility;
 
-import com.casestudy.case_study.model.customer.Customer;
 import com.casestudy.case_study.model.facility.Facility;
 import com.casestudy.case_study.service.IGeneralService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IFacilityService extends IGeneralService<Facility> {
-    Page<Facility> findAll(Pageable pageable);
     Page<Facility> findAll(String searchName, String searchRentType,
                            String searchFacilityType, Pageable pageable);
 }
